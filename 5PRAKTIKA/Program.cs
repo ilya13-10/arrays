37eefa6dfb1d1a6392f8f282b78112d0ae49031c
ilya13-10 <zelenin.ilya2010@yandex.ru>
2019-10-31 00:11:37 +0300

    first commit

---
+++ 5PRAKTIKA/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _5PRAKTIKA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int size = int.Parse(Console.ReadLine());
+            int[] mas = new int[size];
+            for(int i=0;i<mas.Length;i++)
+            {
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < mas.Length; i+=2)
+            {
+                Console.WriteLine(mas[i]);
+            }
+            for (int i = 0; i < mas.Length; i += 2)
+            {
+                if (mas[i] > max)
+                    max = mas[i];
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
